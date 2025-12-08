ec22051ce4722c13de28b8d2e58bd78dddd02515
FraudulentRat <choongyh2005@gmail.com>
2025-12-08 20:08:56 +0800

    fix minor bug on label

--- CW2025-master/src/main/java/com/comp2042/GuiController.java
+++ CW2025-master/src/main/java/com/comp2042/GuiController.java
@@ -48,6 +48,7 @@ public class GuiController implements Initializable {
 
     private Timeline timeLine;
 
+    @FXML
     private Label scoreLabel;
 
     private final BooleanProperty isPause = new SimpleBooleanProperty();
@@ -113,8 +114,8 @@ public class GuiController implements Initializable {
                 brickPanel.add(rectangle, j, i);
             }
         }
-        brickPanel.setLayoutX(gamePanel.getLayoutX() + brick.getxPosition() * brickPanel.getVgap() + brick.getxPosition() * BRICK_SIZE);
-        brickPanel.setLayoutY(-42 + gamePanel.getLayoutY() + brick.getyPosition() * brickPanel.getHgap() + brick.getyPosition() * BRICK_SIZE);
+        brickPanel.setLayoutX(gamePanel.getLayoutX() + brick.getxPosition() * BRICK_SIZE);
+        brickPanel.setLayoutY(gamePanel.getLayoutY() + (brick.getyPosition()-2)* BRICK_SIZE);
 
 
         timeLine = new Timeline(new KeyFrame(
@@ -162,8 +163,10 @@ public class GuiController implements Initializable {
 
     private void refreshBrick(ViewData brick) {
         if (isPause.getValue() == Boolean.FALSE) {
-            brickPanel.setLayoutX(gamePanel.getLayoutX() + brick.getxPosition() * brickPanel.getVgap() + brick.getxPosition() * BRICK_SIZE);
-            brickPanel.setLayoutY(-42 + gamePanel.getLayoutY() + brick.getyPosition() * brickPanel.getHgap() + brick.getyPosition() * BRICK_SIZE);
+            double x = gamePanel.getLayoutX() + brick.getxPosition() * BRICK_SIZE;
+            double y = gamePanel.getLayoutY() + (brick.getyPosition()-2) * BRICK_SIZE;
+            brickPanel.setLayoutX(x);
+            brickPanel.setLayoutY(y);
             for (int i = 0; i < brick.getBrickData().length; i++) {
                 for (int j = 0; j < brick.getBrickData()[i].length; j++) {
                     setRectangleData(brick.getBrickData()[i][j], rectangles[i][j]);
